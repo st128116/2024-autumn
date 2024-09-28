238a181ebec7bfae4c72e7e4ce00953b56b605c9
st128116 <zhva1997@gmail.com>
2024-09-28 23:55:00 +0300

    Update Source.cpp

--- 2024.09.21-HW-1/Project4/Source.cpp
+++ 2024.09.21-HW-1/Project4/Source.cpp
@@ -2,7 +2,9 @@
 
 int main(int argc, char* argv[])
 {
-	short int n, a, b;
+	short int n = 0;
+	short int a = 0;
+	short int b = 0;
 	scanf_s("%hd %hd %hd", &n, &a, &b);
 	printf("%hd", n * a * b * 2);
 	return EXIT_SUCCESS;
