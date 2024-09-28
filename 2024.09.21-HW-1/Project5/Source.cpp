80ef9378dc2ebbd2f78d26bec5b4386e2c63db3f
st128116 <zhva1997@gmail.com>
2024-09-28 23:55:16 +0300

    Update Source.cpp

--- 2024.09.21-HW-1/Project5/Source.cpp
+++ 2024.09.21-HW-1/Project5/Source.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char* argv[])
 {
-	short int a;
+	short int a = 0;
 	scanf_s("%hd", &a);
 	printf("The next number for the number %hd is %hd.\n", a, a + 1);
 	printf("The previous number for the number% hd is% hd.\n", a, a - 1);
