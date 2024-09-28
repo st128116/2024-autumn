86a5500ab101b52ac19e3ce7d87e967c085e1b88
st128116 <zhva1997@gmail.com>
2024-09-28 23:57:07 +0300

    Update Source.cpp

--- 2024.09.21-HW-1/Project9/Source.cpp
+++ 2024.09.21-HW-1/Project9/Source.cpp
@@ -2,7 +2,9 @@
 
 int main(int argc, char* argv[])
 {
-	short int h, a, b;
+	short int h = 0;
+	short int a = 0;
+	short int b = 0;
 	scanf_s("%hd %hd %hd", &h, &a, &b);
 	short int h_a = h - a;
 	short int a_b = a - b;
